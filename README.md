81e6f9246c9a34b00e4ba8e339bd1c27fccc3c50
ezequiel <ezequiel.valdez-non-empl@moodys.com>
2021-04-30 18:16:48 -0300

    master - add linter to ui

---
+++ README.md
@@ -0,0 +1,14 @@
+# Blank Wallet Extension
+
+## How to clone 
+The first time you need to type
+
+`git clone https://github.com/Blank-Wallet/extension`
+
+`git submodule update --init --recursive`
+
+## How to build
+
+Run in the root folder
+`make build`
+
--- scripts/ui/config/webpack.config.js
+++ scripts/ui/config/webpack.config.js
@@ -26,6 +26,7 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const PrettierPlugin = require("prettier-webpack-plugin");
 
 const postcssNormalize = require('postcss-normalize');
 
@@ -729,6 +730,7 @@ module.exports = function (webpackEnv) {
           },
         },
       }),
+      new PrettierPlugin()
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.

8f37c7a66256d871e7f5ce8570e2f5ca2279da86
Facundo Pezzola <fpezzola@users.noreply.github.com>
2022-09-13 13:49:13 +0200

    fix: block fetch sleep

--- packages/background/src/utils/blockFetch.ts
+++ packages/background/src/utils/blockFetch.ts
@@ -4,7 +4,7 @@ import { Block } from '@ethersproject/abstract-provider';
 import log from 'loglevel';
 import { sleep } from './sleep';
 
-const FETCH_BLOCK_SLEEP = 4 * MILISECOND;
+const FETCH_BLOCK_SLEEP = 400 * MILISECOND;
 const MAX_REQUEST_RETRY = 10;
 
 export async function fetchBlockWithRetries(

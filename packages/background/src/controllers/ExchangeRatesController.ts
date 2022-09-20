8610504af7a43d0b0892ac957d9a74521300e263
Martin Lecam <martin.lecam@blockwallet.io>
2022-09-20 18:16:08 -0300

    feat: adding chainlink logic

--- packages/background/src/controllers/ExchangeRatesController.ts
+++ packages/background/src/controllers/ExchangeRatesController.ts
@@ -18,6 +18,431 @@ import { ActionIntervalController } from './block-updates/ActionIntervalControll
 import BlockUpdatesController, {
     BlockUpdatesEvents,
 } from './block-updates/BlockUpdatesController';
+import { BigNumber, Contract } from 'ethers';
+import { formatUnits } from 'ethers/lib/utils';
+
+const CHAINLINK_DATAFEEDS_CONTRACTS = {
+    'ETH/USD': {
+        address: '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419',
+        abi: [
+            {
+                inputs: [
+                    {
+                        internalType: 'address',
+                        name: '_aggregator',
+                        type: 'address',
+                    },
+                    {
+                        internalType: 'address',
+                        name: '_accessController',
+                        type: 'address',
+                    },
+                ],
+                stateMutability: 'nonpayable',
+                type: 'constructor',
+            },
+            {
+                anonymous: false,
+                inputs: [
+                    {
+                        indexed: true,
+                        internalType: 'int256',
+                        name: 'current',
+                        type: 'int256',
+                    },
+                    {
+                        indexed: true,
+                        internalType: 'uint256',
+                        name: 'roundId',
+                        type: 'uint256',
+                    },
+                    {
+                        indexed: false,
+                        internalType: 'uint256',
+                        name: 'updatedAt',
+                        type: 'uint256',
+                    },
+                ],
+                name: 'AnswerUpdated',
+                type: 'event',
+            },
+            {
+                anonymous: false,
+                inputs: [
+                    {
+                        indexed: true,
+                        internalType: 'uint256',
+                        name: 'roundId',
+                        type: 'uint256',
+                    },
+                    {
+                        indexed: true,
+                        internalType: 'address',
+                        name: 'startedBy',
+                        type: 'address',
+                    },
+                    {
+                        indexed: false,
+                        internalType: 'uint256',
+                        name: 'startedAt',
+                        type: 'uint256',
+                    },
+                ],
+                name: 'NewRound',
+                type: 'event',
+            },
+            {
+                anonymous: false,
+                inputs: [
+                    {
+                        indexed: true,
+                        internalType: 'address',
+                        name: 'from',
+                        type: 'address',
+                    },
+                    {
+                        indexed: true,
+                        internalType: 'address',
+                        name: 'to',
+                        type: 'address',
+                    },
+                ],
+                name: 'OwnershipTransferRequested',
+                type: 'event',
+            },
+            {
+                anonymous: false,
+                inputs: [
+                    {
+                        indexed: true,
+                        internalType: 'address',
+                        name: 'from',
+                        type: 'address',
+                    },
+                    {
+                        indexed: true,
+                        internalType: 'address',
+                        name: 'to',
+                        type: 'address',
+                    },
+                ],
+                name: 'OwnershipTransferred',
+                type: 'event',
+            },
+            {
+                inputs: [],
+                name: 'acceptOwnership',
+                outputs: [],
+                stateMutability: 'nonpayable',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'accessController',
+                outputs: [
+                    {
+                        internalType: 'contract AccessControllerInterface',
+                        name: '',
+                        type: 'address',
+                    },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'aggregator',
+                outputs: [
+                    { internalType: 'address', name: '', type: 'address' },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [
+                    {
+                        internalType: 'address',
+                        name: '_aggregator',
+                        type: 'address',
+                    },
+                ],
+                name: 'confirmAggregator',
+                outputs: [],
+                stateMutability: 'nonpayable',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'decimals',
+                outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'description',
+                outputs: [{ internalType: 'string', name: '', type: 'string' }],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [
+                    {
+                        internalType: 'uint256',
+                        name: '_roundId',
+                        type: 'uint256',
+                    },
+                ],
+                name: 'getAnswer',
+                outputs: [{ internalType: 'int256', name: '', type: 'int256' }],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [
+                    {
+                        internalType: 'uint80',
+                        name: '_roundId',
+                        type: 'uint80',
+                    },
+                ],
+                name: 'getRoundData',
+                outputs: [
+                    { internalType: 'uint80', name: 'roundId', type: 'uint80' },
+                    { internalType: 'int256', name: 'answer', type: 'int256' },
+                    {
+                        internalType: 'uint256',
+                        name: 'startedAt',
+                        type: 'uint256',
+                    },
+                    {
+                        internalType: 'uint256',
+                        name: 'updatedAt',
+                        type: 'uint256',
+                    },
+                    {
+                        internalType: 'uint80',
+                        name: 'answeredInRound',
+                        type: 'uint80',
+                    },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [
+                    {
+                        internalType: 'uint256',
+                        name: '_roundId',
+                        type: 'uint256',
+                    },
+                ],
+                name: 'getTimestamp',
+                outputs: [
+                    { internalType: 'uint256', name: '', type: 'uint256' },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'latestAnswer',
+                outputs: [{ internalType: 'int256', name: '', type: 'int256' }],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'latestRound',
+                outputs: [
+                    { internalType: 'uint256', name: '', type: 'uint256' },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'latestRoundData',
+                outputs: [
+                    { internalType: 'uint80', name: 'roundId', type: 'uint80' },
+                    { internalType: 'int256', name: 'answer', type: 'int256' },
+                    {
+                        internalType: 'uint256',
+                        name: 'startedAt',
+                        type: 'uint256',
+                    },
+                    {
+                        internalType: 'uint256',
+                        name: 'updatedAt',
+                        type: 'uint256',
+                    },
+                    {
+                        internalType: 'uint80',
+                        name: 'answeredInRound',
+                        type: 'uint80',
+                    },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'latestTimestamp',
+                outputs: [
+                    { internalType: 'uint256', name: '', type: 'uint256' },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'owner',
+                outputs: [
+                    {
+                        internalType: 'address payable',
+                        name: '',
+                        type: 'address',
+                    },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [{ internalType: 'uint16', name: '', type: 'uint16' }],
+                name: 'phaseAggregators',
+                outputs: [
+                    {
+                        internalType: 'contract AggregatorV2V3Interface',
+                        name: '',
+                        type: 'address',
+                    },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'phaseId',
+                outputs: [{ internalType: 'uint16', name: '', type: 'uint16' }],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [
+                    {
+                        internalType: 'address',
+                        name: '_aggregator',
+                        type: 'address',
+                    },
+                ],
+                name: 'proposeAggregator',
+                outputs: [],
+                stateMutability: 'nonpayable',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'proposedAggregator',
+                outputs: [
+                    {
+                        internalType: 'contract AggregatorV2V3Interface',
+                        name: '',
+                        type: 'address',
+                    },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [
+                    {
+                        internalType: 'uint80',
+                        name: '_roundId',
+                        type: 'uint80',
+                    },
+                ],
+                name: 'proposedGetRoundData',
+                outputs: [
+                    { internalType: 'uint80', name: 'roundId', type: 'uint80' },
+                    { internalType: 'int256', name: 'answer', type: 'int256' },
+                    {
+                        internalType: 'uint256',
+                        name: 'startedAt',
+                        type: 'uint256',
+                    },
+                    {
+                        internalType: 'uint256',
+                        name: 'updatedAt',
+                        type: 'uint256',
+                    },
+                    {
+                        internalType: 'uint80',
+                        name: 'answeredInRound',
+                        type: 'uint80',
+                    },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'proposedLatestRoundData',
+                outputs: [
+                    { internalType: 'uint80', name: 'roundId', type: 'uint80' },
+                    { internalType: 'int256', name: 'answer', type: 'int256' },
+                    {
+                        internalType: 'uint256',
+                        name: 'startedAt',
+                        type: 'uint256',
+                    },
+                    {
+                        internalType: 'uint256',
+                        name: 'updatedAt',
+                        type: 'uint256',
+                    },
+                    {
+                        internalType: 'uint80',
+                        name: 'answeredInRound',
+                        type: 'uint80',
+                    },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                inputs: [
+                    {
+                        internalType: 'address',
+                        name: '_accessController',
+                        type: 'address',
+                    },
+                ],
+                name: 'setController',
+                outputs: [],
+                stateMutability: 'nonpayable',
+                type: 'function',
+            },
+            {
+                inputs: [
+                    { internalType: 'address', name: '_to', type: 'address' },
+                ],
+                name: 'transferOwnership',
+                outputs: [],
+                stateMutability: 'nonpayable',
+                type: 'function',
+            },
+            {
+                inputs: [],
+                name: 'version',
+                outputs: [
+                    { internalType: 'uint256', name: '', type: 'uint256' },
+                ],
+                stateMutability: 'view',
+                type: 'function',
+            },
+        ],
+    },
+};
 
 export interface ExchangeRatesControllerState {
     exchangeRates: Rates;
@@ -141,13 +566,48 @@ export class ExchangeRatesController extends BaseController<ExchangeRatesControl
             ];
 
         // Get network native currency rate
-        const nativeCurrencyRates = (
-            await this._getNetworkNativeCurrencyRate()
-        )[currencyApiId];
+        //  - If preferencesController.nativeCurrency is USD --> fetch ETH/USD price from ChainLink data feed
+        //  - For the rest of the assets, keep fetching them from Coingecko for now.
+        const { chainId } = this._networkController.network;
+        const nativeCurrency =
+            this._preferencesController.nativeCurrency.toLowerCase();
 
-        // Response -> { ethereum : { [nativeCurrency] : rate }}
-        rates[symbol] =
-            nativeCurrencyRates[this._preferencesController.nativeCurrency];
+        //chainId === 1 &&
+        if (nativeCurrency == 'usd') {
+            const dataFeed = CHAINLINK_DATAFEEDS_CONTRACTS['ETH/USD'];
+            const chainLinkDataFeedContract = new Contract(
+                dataFeed.address,
+                dataFeed.abi,
+                //TODO: check if we want to get the price using the mainnet datafeed even when standing on other networks
+                this._networkController.getProviderFromName('mainnet')
+                // this._networkController.getProvider()
+            );
+
+            const roundData = await chainLinkDataFeedContract.latestRoundData();
+            const price = parseFloat(
+                formatUnits(BigNumber.from(roundData.answer), 8)
+            );
+            console.log('Latest Round Data', price);
+
+            // rates[symbol] = price; // Data Feed price.
+            rates['eth'] = price; // Data Feed price.
+        }
+
+        // If native token is not ETH, we fetch the price from coingecko
+        if (symbol !== 'eth') {
+            const nativeCurrencyRates = (
+                await this._getNetworkNativeCurrencyRate()
+            )[currencyApiId];
+            // Response -> { ethereum : { [nativeCurrency] : rate }}
+            rates[symbol] =
+                nativeCurrencyRates[this._preferencesController.nativeCurrency];
+        }
+
+        // TODO:
+        // - Chequear el symbol en distintas networks y si es ETH asegurarse q se fetchea del feed
+        // - En el fetch del resto de los tokens, NO pisar el valor de ETH calculado anteriorimente (en las networks q eth no es nativecurrency).
+        // - Una vez q funca todo, abstraer logica a un metodo lo mas "generico" posible por si agregamos mas feeds
+        //    - por ej instanciar el contract solo en el constructor
 
         // Get tokens exchange rates
         const tokenRatesQuery = await this._getTokenRates();

dbb327ce5abe83fc1a8f17c3384505a901d5372e
leablock <98833290+leablock@users.noreply.github.com>
2022-10-14 12:55:11 -0300

    Chore: Skip pipeline in Release PR and some minor fixes (#164)
    * chore: skip the CI workflow when there's a pr from the release process
    
    * chore: test new workflow
    
    * chore: test
    
    * Revert "chore: test new workflow"
    
    This reverts commit 8105a747daed77f0194800ec0213830f5b648158.
    
    * Revert "chore: test"
    
    This reverts commit e049924e23a5675865abeaa4f4b49b4eabdf673d.
    
    * chore: test old CI
    
    * chore: build requires lint
    
    * chore: remove paths
    
    * chore: update fake CI
    
    * Revert "chore: test old CI"
    
    This reverts commit d75ef59f7930c892bc181a88442b799c82863e55.
    
    * chore: test
    
    * chore: test
    
    * Revert "chore: test"
    
    This reverts commit 3e038263bb36fc225bbd3f930f217fab552af403.
    
    * Revert "chore: test"
    
    This reverts commit e0812272f0bc816d52cdbcfda3b0b7e362c8de16.
    
    * chore: test
    
    * chore: test
    
    * chore: delete old files
    
    * chore: restore file
    
    * chore: fix pipeline
    
    * chore: test
    
    * chore: test
    
    * chore: test
    
    * chore: test
    
    * chore: update pipeline due to new trigger
    
    * chore: test
    
    * Revert "chore: test"
    
    This reverts commit cb51bc2e172b99a69ce7bb2a407d526835714bd7.
    
    * chore: test
    
    * fix: makefile
    
    * Revert "chore: test"
    
    This reverts commit c4e3bb93875411402436670b3788804d3bf2c3cf.

--- .make/includes/guard.mk
+++
@@ -1,6 +0,0 @@
-guard-%:
-	@ if [ "${${*}}" = "" ]; then \
-		echo "\n👉 ${YELLOW}Environment variable ${GREEN}$*${YELLOW} not set (make $*=.. target or export $*=..${RESET}"; \
-		echo "\n👉 Run ${GREEN}make help${RESET} for options..\n"; \
-		exit 1; \
-	fi

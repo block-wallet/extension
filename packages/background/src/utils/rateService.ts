a136b5f7b3ff86d20c44fda62f746006b0a638f2
Martin Lecam <martin.lecam@blockwallet.io>
2022-10-18 16:41:06 -0300

    fix: Add try catch in both services, coingeko and chainlink

--- packages/background/src/utils/rateService.ts
+++ packages/background/src/utils/rateService.ts
@@ -20,46 +20,62 @@ export const BaseApiEndpoint = 'https://api.coingecko.com/api/v3/simple/';
 
 export const chainLinkService: RateService = {
     async getRate(currency, symbol, options: getRateOptions = {}) {
-        const dataFeedPair =
-            symbol.toUpperCase() + '/' + currency.toUpperCase();
-        const dataFeedAddress =
-            CHAINLINK_DATAFEEDS_CONTRACTS[dataFeedPair]?.address;
-        const dataFeedABI = CHAINLINK_DATAFEEDS_CONTRACTS[dataFeedPair]?.abi;
+        try {
+            const dataFeedPair =
+                symbol.toUpperCase() + '/' + currency.toUpperCase();
+            const dataFeedAddress =
+                CHAINLINK_DATAFEEDS_CONTRACTS[dataFeedPair]?.address;
+            const dataFeedABI =
+                CHAINLINK_DATAFEEDS_CONTRACTS[dataFeedPair]?.abi;
 
-        if (dataFeedAddress && dataFeedABI) {
-            const chainLinkDataFeedContract = new Contract(
-                dataFeedAddress,
-                dataFeedABI,
-                options.networkProvider
-            );
+            if (dataFeedAddress && dataFeedABI) {
+                const chainLinkDataFeedContract = new Contract(
+                    dataFeedAddress,
+                    dataFeedABI,
+                    options.networkProvider
+                );
 
-            const roundData = await chainLinkDataFeedContract.latestRoundData();
-            return parseFloat(formatUnits(BigNumber.from(roundData.answer), 8));
+                const roundData =
+                    await chainLinkDataFeedContract.latestRoundData();
+                return parseFloat(
+                    formatUnits(BigNumber.from(roundData.answer), 8)
+                );
+            }
+
+            //At this point there is no dataFeedPair in CHAINLINK_DATAFEEDS_CONTRACTS
+            return 0;
+        } catch (e) {
+            console.log('Failed fecthing price from ChainLink. Ex: ' + e);
+            return 0;
         }
-
-        //At this point there is no dataFeedPair in CHAINLINK_DATAFEEDS_CONTRACTS
-        return 0;
     },
 };
 
 export const coingekoService: RateService = {
     async getRate(currency, symbol) {
-        const query = `${BaseApiEndpoint}price`;
-        const currencyApiId =
-            RATES_IDS_LIST[symbol.toUpperCase() as keyof typeof RATES_IDS_LIST];
+        try {
+            const query = `${BaseApiEndpoint}price`;
+            const currencyApiId =
+                RATES_IDS_LIST[
+                    symbol.toUpperCase() as keyof typeof RATES_IDS_LIST
+                ];
 
-        const response = await axios.get(query, {
-            params: {
-                ids: currencyApiId,
-                vs_currencies: currency,
-            },
-        });
+            const response = await axios.get(query, {
+                params: {
+                    ids: currencyApiId,
+                    vs_currencies: currency,
+                },
+            });
 
-        if (response.status != 200) {
-            throw new Error(response.statusText);
+            if (response.status != 200) {
+                throw new Error(response.statusText);
+            }
+
+            return response.data[currencyApiId][currency];
+        } catch (e) {
+            console.log('Failed fecthing price from Coingeko. Ex: ' + e);
+            return 0;
         }
-
-        return response.data[currencyApiId][currency];
     },
 };
 

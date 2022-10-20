103ca1169df27e73ff8aaa31129050c3bdc72bc9
Rodrigo <10286502+rodr-r@users.noreply.github.com>
2022-10-20 17:00:18 -0300

    Preventing the user from executing the onboard twice (#176)
    * Preventing the user from execute the onboard twice
    
    * move the duplicated logic to a hook
    
    * prevent the setup notification to be fired multiple times
    
    * fixing typo

---
+++ packages/ui/src/context/hooks/useCheckUserIsOnboarded.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { useBlankState } from "../background/backgroundHooks"
+
+export const useCheckUserIsOnboarded = () => {
+    const history = useHistory()
+    const state = useBlankState()
+    useEffect(() => {
+        if (!state || state.isOnboarded) {
+            history.push({
+                pathname: "/setup/done",
+                state: { sendNotification: false },
+            })
+        }
+    }, [])
+}

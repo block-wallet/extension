c478350b130eba30059798bf40d7938db454addf
Facundo Pezzola <fpezzola@users.noreply.github.com>
2022-10-28 15:03:24 +0200

    fix turn on linter and manually check nulls

--- packages/background/src/utils/incomingBridgePlaceholder.ts
+++ packages/background/src/utils/incomingBridgePlaceholder.ts
@@ -1,26 +1,34 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { BigNumber } from 'ethers';
 import {
     TransactionCategories,
     TransactionMeta,
     TransactionStatus,
+    TransferType,
 } from '../controllers/transactions/utils/types';
 
 const transactionToIncomingBridgeTransactionPlaceholder = (
     sendingTx: TransactionMeta,
     chainId: number
 ): TransactionMeta => {
+    let transferType: TransferType | undefined = undefined;
+    if (
+        sendingTx.bridgeParams &&
+        sendingTx.bridgeParams.toTokenAmount &&
+        sendingTx.bridgeParams.toToken
+    ) {
+        transferType = {
+            amount: BigNumber.from(sendingTx.bridgeParams.toTokenAmount),
+            currency: sendingTx.bridgeParams.toToken.symbol,
+            decimals: sendingTx.bridgeParams.toToken.decimals,
+        };
+    }
     return {
         ...sendingTx,
         id: '',
         transactionParams: {},
         chainId,
         transactionCategory: TransactionCategories.INCOMING_BRIDGE_PLACEHOLDER,
-        transferType: {
-            amount: BigNumber.from(sendingTx.bridgeParams!.toTokenAmount),
-            currency: sendingTx.bridgeParams!.toToken.symbol,
-            decimals: sendingTx.bridgeParams!.toToken.decimals,
-        },
+        transferType,
         status: TransactionStatus.SUBMITTED,
         confirmationTime: undefined,
         methodSignature: undefined,

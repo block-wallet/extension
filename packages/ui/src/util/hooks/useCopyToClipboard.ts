31327ac446cc88048be7bb51d83d40c0e312d802
Facundo Pezzola <22504074+fpezzola@users.noreply.github.com>
2022-11-07 16:04:25 -0300

    [Fix] Transaction details for token transfers (#200)

--- packages/ui/src/util/hooks/useCopyToClipboard.ts
+++ packages/ui/src/util/hooks/useCopyToClipboard.ts
@@ -14,4 +14,18 @@ const useCopyToClipboard = (initialText?: string) => {
     }
 }
 
+export const useMultipleCopyToClipboard = () => {
+    const [copied, setCopied] = useState<number>(-1)
+    return {
+        copied,
+        //you can override the inital text if you want to.
+        onCopy: async (text: string, id: number) => {
+            await navigator.clipboard.writeText(text || "")
+            setCopied(id)
+            await new Promise((resolve) => setTimeout(resolve, 1000))
+            setCopied(-1)
+        },
+    }
+}
+
 export default useCopyToClipboard

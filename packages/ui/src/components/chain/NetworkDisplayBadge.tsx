ee83fe8d15703f5a44a52f5110145c9e5599b060
MartnLecam <106965328+MartnLecam@users.noreply.github.com>
2022-12-12 09:43:14 -0300

    [Fix] Network name switch page (#226)
    * fix: Add same color logic for displaybadge
    
    * fix: function to get network color, will be used for all the pages who want to get that info. Added color to network dropdown. Remove capitalize from getNetworkName. Change width for network dropdown and network badge
    
    * fix: Solve space difference between badge and dropdown. Fix color span in network change window
    
    * fix: add ui fixes. Remove useEffect from switch page
    
    Co-authored-by: Julián Ariel Martínez <julian@blockwallet.io>

--- packages/ui/src/components/chain/NetworkDisplayBadge.tsx
+++ packages/ui/src/components/chain/NetworkDisplayBadge.tsx
@@ -1,56 +1,59 @@
 import { Network } from "@block-wallet/background/utils/constants/networks"
 import { classnames } from "../../styles"
+import { getNetworkColor } from "../../util/getNetworkColor"
 import GenericTooltip from "../label/GenericTooltip"
 
 const NetworkDisplayBadge = ({
     network,
-    iconColor = "bg-green-500",
     className,
     fill = false,
     truncate = false,
 }: {
-    network: Network | string
-    iconColor?: string
+    network: Network
     fill?: boolean
     truncate?: boolean
     className?: string
 }) => {
-    const networkName = typeof network === "string" ? network : network.desc
+    const networkDesc = network.desc
+    const networkColor = getNetworkColor(network)
 
     // We limit the string chars here regardless of the truncate flag
     // and the truncate class to prevent issues with the animated bullet size
     // and to have a limit on huge network descriptions.
     const formattedNetworkName = truncate
-        ? networkName.length > 42
-            ? `${networkName.slice(0, 42)}...`
-            : networkName
-        : networkName
+        ? networkDesc.length > 42
+            ? `${networkDesc.slice(0, 42)}...`
+            : networkDesc
+        : networkDesc
     return (
         <div
             className={classnames(
-                "flex flex-row items-center justify-center space-x-1 py-1.5 px-1.5 text-gray-600 rounded-md group border border-primary-200 text-xs",
+                "flex flex-row items-center py-1.5 px-1.5 text-gray-600 rounded-md group border border-primary-200 text-xs",
                 fill && "bg-green-100",
                 "w-auto",
                 className
             )}
         >
             <span
-                className={`rounded-full h-2 w-2  animate-pulse ${iconColor} pointer-events-none`}
+                className={
+                    "justify-end h-2 rounded-xl mr-2 ml-1 animate-pulse pointer-events-none"
+                }
+                style={{ backgroundColor: networkColor, width: "8px" }}
             />
             <span
                 className={classnames(
                     truncate
-                        ? "truncate ..."
-                        : "whitespace-normal break-all flex-1"
+                        ? "flex justify-center truncate ..."
+                        : "flex justify-center whitespace-normal break-all flex-1"
                 )}
-                style={truncate ? { maxWidth: 110 } : {}}
+                style={truncate ? { maxWidth: 130 } : {}}
             >
                 {formattedNetworkName}
             </span>
             <GenericTooltip
                 left
                 className="translate-y-5 translate-x-3 shadow-md min-w-max"
-                content={<p className=" p-1 text-left">{networkName}</p>}
+                content={<p className=" p-1 text-left">{networkDesc}</p>}
             />
         </div>
     )

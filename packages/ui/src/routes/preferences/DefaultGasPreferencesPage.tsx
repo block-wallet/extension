20b55ba1bd30c02046d6d162e3304a6244c1c964
Julian Ariel Martinez <julymartinez94@gmail.com>
2023-01-06 11:25:26 -0300

    feat: finished new screen and setting update.

--- packages/ui/src/routes/preferences/DefaultGasPreferencesPage.tsx
+++ packages/ui/src/routes/preferences/DefaultGasPreferencesPage.tsx
@@ -1,40 +1,107 @@
+import { DefaultGasOptions } from "@block-wallet/background/controllers/PreferencesController"
 import classnames from "classnames"
+import { useCallback, useState } from "react"
+import { useHistory } from "react-router-dom"
 import { ButtonWithLoading } from "../../components/button/ButtonWithLoading"
+import SuccessDialog from "../../components/dialog/SuccessDialog"
+import MiniCheckmark from "../../components/icons/MiniCheckmark"
+import InfoComponent from "../../components/InfoComponent"
 import PopupFooter from "../../components/popup/PopupFooter"
 import PopupHeader from "../../components/popup/PopupHeader"
 import PopupLayout from "../../components/popup/PopupLayout"
+import { useBlankState } from "../../context/background/backgroundHooks"
+import { setDefaultGasPreference } from "../../context/commActions"
 
+const gasOptions = [
+    { name: "high", desc: "Pricier but faster" },
+    { name: "medium", desc: "Balance price and speed" },
+    { name: "low", desc: "Cheaper but slower" },
+]
 const DefaultGasPreferencesPage = () => {
+    const history = useHistory()
+
+    const defaultGasOption = useBlankState()?.defaultGasOption || "medium"
+    const [selectedOption, setSelectedOption] =
+        useState<DefaultGasOptions>(defaultGasOption)
+    const [isLoading, setIsLoading] = useState(false)
+    const [showSuccessDialog, setShowSuccessDialog] = useState(false)
+
+    const onSave = useCallback(async () => {
+        try {
+            setIsLoading(true)
+            await setDefaultGasPreference(selectedOption)
+            setShowSuccessDialog(true)
+        } catch (e) {
+            throw new Error("Could not update the default gas option")
+        } finally {
+            setIsLoading(false)
+        }
+    }, [selectedOption])
+
     return (
         <PopupLayout
             header={<PopupHeader title="Default Gas Setting" />}
             footer={
                 <PopupFooter>
-                    <ButtonWithLoading label="Save" />
+                    <ButtonWithLoading
+                        label="Save"
+                        isLoading={isLoading}
+                        disabled={defaultGasOption === selectedOption}
+                        onClick={onSave}
+                    />
                 </PopupFooter>
             }
         >
-            <div className="flex flex-col p-6 space-y-6 w-full">
+            <SuccessDialog
+                open={showSuccessDialog}
+                title="Default Gas Option"
+                timeout={800}
+                message="Your changes have been succesfully saved!"
+                onDone={history.goBack}
+            />
+            <div className="flex flex-col p-6 space-y-4 w-full">
                 <span className="text-sm text-gray-500">
                     Set your preferred gas setting for all future transactions.
                 </span>
-                <div className="flex flex-col w-full space-y-2">
-                    <div
-                        //key={option.label}
-                        className="w-full flex flex-col space-y-2 items-center p-4 cursor-pointer rounded-md hover:bg-gray-100 border"
-                        onClick={() => {}}
-                    >
-                        <label
+                <div className="flex flex-col w-full space-y-4">
+                    {gasOptions.map((option) => (
+                        <div
+                            key={option.name}
                             className={classnames(
-                                "text-base font-semibold cursor-pointer capitalize"
-                                //selectedOption.label === option.label &&
-                                //    "text-primary-300"
+                                "w-full flex flex-row p-4 justify-between cursor-pointer rounded-md hover:border-primary-300 hover:text-primary-300 border",
+                                selectedOption === option.name &&
+                                    "text-primary-300 border-primary-300"
                             )}
+                            onClick={() => {
+                                setSelectedOption(
+                                    option.name as DefaultGasOptions
+                                )
+                            }}
                         >
-                            Low{/*{option.label}*/}
-                        </label>
-                        <span className="text-sm">Cheap</span>
-                    </div>
+                            <div className="flex flex-col space-y-1">
+                                <label
+                                    className={classnames(
+                                        "text-sm font-semibold cursor-pointer capitalize"
+                                    )}
+                                >
+                                    {option.name}
+                                </label>
+                                <span className="text-xs">{option.desc}</span>
+                            </div>
+                            {selectedOption === option.name && (
+                                <div className="h-full flex items-center">
+                                    <MiniCheckmark fill="#1673FF" />
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                </div>
+                <div className="flex flex-row space-x-2 w-full ">
+                    <InfoComponent>
+                        This setting will apply on all transactions on all
+                        networks. You will still be able to change the gas
+                        amount before submitting your transactions.
+                    </InfoComponent>
                 </div>
             </div>
         </PopupLayout>

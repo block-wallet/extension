7908a6e970edde4ab21de3ac6bc72c80f50f0258
Rodrigo <10286502+rodr-r@users.noreply.github.com>
2023-01-09 08:47:30 -0300

    Fix/token transactions list filter

--- packages/ui/src/util/hooks/useTokenTransactions.ts
+++ packages/ui/src/util/hooks/useTokenTransactions.ts
@@ -1,12 +1,22 @@
+import { isNativeTokenAddress } from "../tokenUtils"
+import { RichedTransactionMeta } from "../transactionUtils"
 import useTransactions from "./useTransactions"
 
 const useTokenTransactions = (tokenContract: string | undefined) => {
+    if (!tokenContract) {
+        return [] as RichedTransactionMeta[]
+    }
     const contractToLower = tokenContract?.toLowerCase()
     const { transactions } = useTransactions()
-    return transactions.filter(({ transactionParams }) => {
-        return contractToLower
-            ? transactionParams.to?.toLowerCase() === contractToLower
-            : false
+    return transactions.filter(({ transactionParams, transactionReceipt }) => {
+        if (isNativeTokenAddress(tokenContract)) {
+            return transactionParams.data === "0x"
+        } else {
+            return (
+                transactionReceipt?.contractAddress.toLowerCase() ==
+                contractToLower
+            )
+        }
     })
 }
 

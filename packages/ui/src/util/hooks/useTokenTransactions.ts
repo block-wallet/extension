4aebe42993f2e784360cc537a89edc38ac608bc7
Rodrigo <10286502+rodr-r@users.noreply.github.com>
2023-01-09 09:02:35 -0300

    catching errors

--- packages/ui/src/util/hooks/useTokenTransactions.ts
+++ packages/ui/src/util/hooks/useTokenTransactions.ts
@@ -1,23 +1,34 @@
 import { isNativeTokenAddress } from "../tokenUtils"
 import { RichedTransactionMeta } from "../transactionUtils"
 import useTransactions from "./useTransactions"
+import { Token } from "@block-wallet/background/controllers/erc-20/Token"
+import { useBlankState } from "../../context/background/backgroundHooks"
 
-const useTokenTransactions = (tokenContract: string | undefined) => {
-    if (!tokenContract) {
+const useTokenTransactions = (token: Token | undefined) => {
+    if (!token) {
         return [] as RichedTransactionMeta[]
     }
-    const contractToLower = tokenContract?.toLowerCase()
-    const { transactions } = useTransactions()
-    return transactions.filter(({ transactionParams, transactionReceipt }) => {
-        if (isNativeTokenAddress(tokenContract)) {
-            return transactionParams.data === "0x"
-        } else {
-            return (
-                transactionReceipt?.contractAddress.toLowerCase() ==
-                contractToLower
-            )
-        }
-    })
+    try {
+        const contractToLower = token.address.toLowerCase()
+        const { transactions } = useTransactions()
+        return transactions.filter(
+            ({ transactionParams, transactionReceipt, transferType }) => {
+                if (isNativeTokenAddress(token.address)) {
+                    return (
+                        transactionParams.data === "0x" ||
+                        transferType?.currency === token.symbol
+                    )
+                } else {
+                    return (
+                        transactionReceipt?.contractAddress?.toLowerCase() ==
+                        contractToLower
+                    )
+                }
+            }
+        )
+    } catch {
+        return [] as RichedTransactionMeta[]
+    }
 }
 
 export default useTokenTransactions

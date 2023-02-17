5ea12b6d7d2daa03977a4fbf047efae303138979
Antony Ayoub <antony@blockwallet.io>
2023-02-17 23:51:09 +0200

    feat: notifications v0.9

--- packages/background/src/utils/notifications.ts
+++ packages/background/src/utils/notifications.ts
@@ -112,6 +112,7 @@ const getTxNotificationData = (
         status,
         exchangeParams,
         bridgeParams,
+        approveAllowanceParams,
         transferType,
         metaType,
     } = txMeta;
@@ -130,7 +131,7 @@ const getTxNotificationData = (
         url = createCustomExplorerLink(txParams.hash, explorerUrl);
     }
 
-    const isTxFailure = status === TransactionStatus.FAILED;
+    const isTxFailed = status === TransactionStatus.FAILED;
     const isTxCancelled = status === TransactionStatus.CANCELLED;
     const isTxSpedUp =
         status === TransactionStatus.CONFIRMED &&
@@ -138,64 +139,92 @@ const getTxNotificationData = (
 
     if (isTxCancelled) {
         title = `Transaction Cancelled`;
-        message = `Transaction cancelled on ${txNetworkName}.`;
+        message = `Transaction on ${txNetworkName} cancelled.`;
         return { title, message, url };
     }
 
+    let showDefaultNotification = false;
+
     switch (transactionCategory) {
         case TransactionCategories.EXCHANGE:
-            title = `Swap ${isTxFailure ? 'Failed' : 'Completed'}`;
-            message = `${exchangeParams?.fromToken.symbol} to ${
-                exchangeParams?.toToken.symbol
+            if (!exchangeParams) {
+                showDefaultNotification = true;
+                break;
+            }
+            title = `Swap ${isTxFailed ? 'Failed' : 'Completed'}`;
+            message = `${exchangeParams.fromToken.symbol} to ${
+                exchangeParams.toToken.symbol
             } swap on ${txNetworkName} ${
-                isTxFailure ? 'failed' : 'completed'
+                isTxFailed ? 'failed' : 'completed'
             }.`;
             break;
         case TransactionCategories.BRIDGE:
-            title = `Bridge ${isTxFailure ? 'Failed' : 'Completed'}`;
-            message = `${getNetworkData(bridgeParams?.fromChainId)?.name} to ${
-                getNetworkData(bridgeParams?.toChainId)?.name
-            } bridge ${isTxFailure ? 'failed' : 'completed'}.`;
+            if (!bridgeParams) {
+                showDefaultNotification = true;
+                break;
+            }
+            title = `Bridge ${isTxFailed ? 'Failed' : 'Completed'}`;
+            message = `${getNetworkData(bridgeParams.fromChainId)?.name} to ${
+                getNetworkData(bridgeParams.toChainId)?.name
+            } bridge ${isTxFailed ? 'failed' : 'completed'}.`;
             break;
-
-        //TODO: Add notification for token approval when allowance epic is pushed to main branch
-        case TransactionCategories.TOKEN_METHOD_APPROVE:
-            //Token Approval Revoked
-            //<token> approval revoked for <spender> on <network>.
-            title = 'Token Approval';
-            message = `Approved <amount> <token> for use with <spender> on ${txNetworkName}.`;
+        case TransactionCategories.TOKEN_METHOD_APPROVE: {
+            if (!approveAllowanceParams) {
+                showDefaultNotification = true;
+                break;
+            }
+            const { spenderAddress, spenderInfo, token, allowanceValue } =
+                approveAllowanceParams;
+            const isRevoke = BigNumber.from(allowanceValue).eq(0);
+            title = `Token Approval ${isRevoke ? 'Revoke' : ''} ${
+                isTxFailed ? 'Failed' : 'Completed'
+            }`;
+            message = `${token.symbol} approval for ${
+                spenderInfo?.name ??
+                `Spender (${spenderAddress?.slice(
+                    0,
+                    6
+                )}...${spenderAddress?.slice(spenderAddress.length - 4)})`
+            } on ${txNetworkName} ${isTxFailed ? 'failed' : 'completed'}.`;
             break;
+        }
         case TransactionCategories.SENT_ETHER:
-            title = `Transaction ${isTxFailure ? 'Failed' : 'Completed'}`;
+            title = `Transaction ${isTxFailed ? 'Failed' : 'Completed'}`;
             message = `${
                 txNetworkNativeToken.symbol
             } transaction on ${txNetworkName} ${
-                isTxFailure ? 'failed' : 'completed'
+                isTxFailed ? 'failed' : 'completed'
             }.`;
             break;
         case TransactionCategories.TOKEN_METHOD_TRANSFER:
-            title = `Transaction ${isTxFailure ? 'Failed' : 'Completed'}`;
+            if (!transferType) {
+                showDefaultNotification = true;
+                break;
+            }
+            title = `Transaction ${isTxFailed ? 'Failed' : 'Completed'}`;
             message = `${
-                transferType?.currency
+                transferType.currency
             } transaction on ${txNetworkName} ${
-                isTxFailure ? 'failed' : 'completed'
+                isTxFailed ? 'failed' : 'completed'
             }.`;
             break;
 
         case TransactionCategories.TOKEN_METHOD_INCOMING_TRANSFER: {
+            if (!transferType && !txParams) {
+                showDefaultNotification = true;
+                break;
+            }
             const isNativeToken = !transferType;
 
             const decimals = isNativeToken
-                ? txNetworkNativeToken?.decimals
-                : transferType?.decimals;
+                ? txNetworkNativeToken.decimals
+                : transferType.decimals;
 
             const symbol = isNativeToken
-                ? txNetworkNativeToken?.symbol
-                : transferType?.currency;
+                ? txNetworkNativeToken.symbol
+                : transferType.currency;
 
-            const value = isNativeToken
-                ? txParams?.value
-                : transferType?.amount;
+            const value = isNativeToken ? txParams.value : transferType.amount;
 
             title = 'Incoming Transaction';
             message = `Received ${formatTokenAmount(
@@ -206,27 +235,26 @@ const getTxNotificationData = (
             break;
         }
         case TransactionCategories.CONTRACT_INTERACTION:
-            title = `Contract Interaction ${isTxFailure ? 'Failed' : ''}`;
+            title = `Contract Interaction ${isTxFailed ? 'Failed' : ''}`;
             message = `Interaction with contract on ${txNetworkName} ${
-                isTxFailure ? 'failed' : 'completed'
+                isTxFailed ? 'failed' : 'completed'
             }.`;
             break;
         case TransactionCategories.CONTRACT_DEPLOYMENT:
             title = `Contract ${
-                isTxFailure ? 'Deployment Failed' : 'Deployed'
+                isTxFailed ? 'Deployment Failed' : 'Deployed'
             }`;
             message = `Contract deployment on ${txNetworkName} ${
-                isTxFailure ? 'failed' : 'completed'
+                isTxFailed ? 'failed' : 'completed'
             }.`;
             break;
-        default:
-            title = isTxFailure
-                ? 'Transaction Failed'
-                : 'Transaction Completed';
-            message = `Transaction on ${txNetworkName} ${
-                isTxFailure ? 'failed' : 'completed'
-            }.`;
-            break;
+    }
+
+    if (showDefaultNotification) {
+        title = isTxFailed ? 'Transaction Failed' : 'Transaction Completed';
+        message = `Transaction on ${txNetworkName} ${
+            isTxFailed ? 'failed' : 'completed'
+        }.`;
     }
 
     if (isTxSpedUp) {

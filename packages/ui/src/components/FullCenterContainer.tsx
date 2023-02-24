bca5c3d0aad3433e1b26dfc5e011a939016a6537
Julian Ariel Martinez <julymartinez94@gmail.com>
2023-02-24 17:27:54 -0300

    style: fixed experimental version dialogs.

--- packages/ui/src/components/FullCenterContainer.tsx
+++ packages/ui/src/components/FullCenterContainer.tsx
@@ -1,7 +1,8 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent, useState } from "react"
 
 import classnames from "classnames"
 import WarningTip from "./label/WarningTip"
+import IsBetaDialog from "./dialog/IsBetaDialog"
 
 const FullCenterContainer: FunctionComponent<{
     centered?: boolean
@@ -13,37 +14,48 @@ const FullCenterContainer: FunctionComponent<{
     centered = false,
     screen = false,
     displayWarningTip = false,
-}) => (
-    <>
-        {displayWarningTip && (
-            <WarningTip
-                text={
-                    <>
-                        <b>Warning!</b> This is an experimental version of
-                        BlockWallet
-                    </>
-                }
-                fontSize="text-sm"
-                justify="justify-center"
-                className="w-full"
+}) => {
+    const [isBetaDiallogOpen, setIsBetaDiallogOpen] = useState(false)
+    return (
+        <>
+            <IsBetaDialog
+                isOpen={isBetaDiallogOpen}
+                onClick={() => {
+                    setIsBetaDiallogOpen(false)
+                }}
             />
-        )}
-        <div
-            className="w-full min-h-full flex bg-primary-100"
-            style={screen ? {} : { height: "fit-content" }}
-        >
+            {displayWarningTip && (
+                <div onClick={() => setIsBetaDiallogOpen(true)}>
+                    <WarningTip
+                        text={
+                            <>
+                                <b>Warning!</b> This is an experimental version
+                                of BlockWallet
+                            </>
+                        }
+                        fontSize="text-sm"
+                        justify="justify-center"
+                        className="w-full cursor-pointer"
+                    />
+                </div>
+            )}
             <div
-                className={classnames(
-                    "flex flex-1 md:flex-0",
-                    !screen && "px-2",
-                    "mx-auto",
-                    centered ? "my-auto" : !screen ? "mb-auto" : ""
-                )}
+                className="w-full min-h-full flex bg-primary-100"
+                style={screen ? {} : { height: "fit-content" }}
             >
-                {children}
+                <div
+                    className={classnames(
+                        "flex flex-1 md:flex-0",
+                        !screen && "px-2",
+                        "mx-auto",
+                        centered ? "my-auto" : !screen ? "mb-auto" : ""
+                    )}
+                >
+                    {children}
+                </div>
             </div>
-        </div>
-    </>
-)
+        </>
+    )
+}
 
 export default FullCenterContainer

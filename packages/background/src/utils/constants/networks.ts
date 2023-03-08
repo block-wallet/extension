13bdc0d02ba0321c6698a161d7acdfdef0e3febc
Antony Ayoub <antony@blockwallet.io>
2023-03-08 13:06:24 +0200

    feat: network indicator redesign Background

--- packages/background/src/utils/constants/networks.ts
+++ packages/background/src/utils/constants/networks.ts
@@ -20,9 +20,10 @@ export type Network = {
         name: string;
         symbol: string;
         decimals: number;
+        logo?: string; // Used if the native currency logo is different from network logo
     };
     hasFixedGasCost?: boolean;
-    iconUrls?: string[];
+    iconUrls?: string[]; // Network logo
     enable: boolean;
     features: BlankSupportedFeatures[];
     test: boolean;
@@ -69,6 +70,7 @@ export interface AddNetworkType {
         symbol: string;
         name?: string;
         decimals?: number;
+        logo?: string;
     };
     rpcUrls?: string[];
     test: boolean;
@@ -163,6 +165,9 @@ export const INITIAL_NETWORKS: Networks = {
         ens: true,
         showGasLevels: true,
         rpcUrls: [`https://mainnet-node.blockwallet.io`],
+        iconUrls: [
+            'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/ethereum/info/logo.png',
+        ],
         defaultRpcUrl: `https://mainnet-node.blockwallet.io`,
         blockExplorerUrls: ['https://etherscan.io'],
         blockExplorerName: 'Etherscan',
@@ -185,6 +190,7 @@ export const INITIAL_NETWORKS: Networks = {
             name: 'Ether',
             symbol: 'ETH',
             decimals: 18,
+            logo: 'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/ethereum/info/logo.png',
         },
         hasFixedGasCost: false,
         enable: true,
@@ -194,6 +200,9 @@ export const INITIAL_NETWORKS: Networks = {
         ens: false,
         showGasLevels: false,
         rpcUrls: ['https://arbitrum-node.blockwallet.io'],
+        iconUrls: [
+            'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/arbitrum/info/logo.png',
+        ],
         defaultRpcUrl: 'https://arbitrum-node.blockwallet.io',
         blockExplorerUrls: ['https://arbiscan.io'],
         blockExplorerName: 'Arbiscan',
@@ -216,6 +225,7 @@ export const INITIAL_NETWORKS: Networks = {
             name: 'Ether',
             symbol: 'ETH',
             decimals: 18,
+            logo: 'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/ethereum/info/logo.png',
         },
         hasFixedGasCost: false,
         gasLowerCap: {
@@ -228,6 +238,9 @@ export const INITIAL_NETWORKS: Networks = {
         ens: false,
         showGasLevels: false,
         rpcUrls: ['https://optimism-node.blockwallet.io'],
+        iconUrls: [
+            'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/optimism/info/logo.png',
+        ],
         defaultRpcUrl: 'https://optimism-node.blockwallet.io',
         blockExplorerUrls: ['https://optimistic.etherscan.io'],
         blockExplorerName: 'Etherscan',
@@ -388,6 +401,7 @@ export const INITIAL_NETWORKS: Networks = {
             name: 'xDAI',
             symbol: 'xDAI',
             decimals: 18,
+            logo: 'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/xdai/assets/0x/logo.png',
         },
         hasFixedGasCost: false,
         enable: true,
@@ -397,7 +411,7 @@ export const INITIAL_NETWORKS: Networks = {
         ens: false,
         showGasLevels: false,
         iconUrls: [
-            'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/xdai/assets/0x/logo.png',
+            'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/xdai/info/logo.png',
         ],
         rpcUrls: ['https://xdai-node.blockwallet.io'],
         defaultRpcUrl: 'https://xdai-node.blockwallet.io',
@@ -460,6 +474,9 @@ export const INITIAL_NETWORKS: Networks = {
         ens: true,
         showGasLevels: true,
         rpcUrls: [`https://goerli-node.blockwallet.io`],
+        iconUrls: [
+            'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/ethereum/info/logo.png',
+        ],
         defaultRpcUrl: `https://goerli-node.blockwallet.io`,
         blockExplorerUrls: ['https://goerli.etherscan.io'],
         blockExplorerName: 'Etherscan',
@@ -541,6 +558,7 @@ export const INITIAL_NETWORKS: Networks = {
             name: 'Ether',
             symbol: 'ETH',
             decimals: 18,
+            logo: 'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/ethereum/info/logo.png',
         },
         iconUrls: [
             'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/zksync/info/logo.png',
@@ -664,6 +682,7 @@ export const INITIAL_NETWORKS: Networks = {
             name: 'Ether',
             symbol: 'ETH',
             decimals: 18,
+            logo: 'https://raw.githubusercontent.com/block-wallet/assets/master/blockchains/ethereum/info/logo.png',
         },
         hasFixedGasCost: false,
         enable: true,

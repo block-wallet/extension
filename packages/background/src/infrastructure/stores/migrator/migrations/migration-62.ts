efb44ec404d180783b883695ac1af6da6bb0af2e
Julian Ariel Martinez <julymartinez94@gmail.com>
2023-03-09 16:23:34 -0300

    lint: fixed lint.

--- packages/background/src/infrastructure/stores/migrator/migrations/migration-62.ts
+++ packages/background/src/infrastructure/stores/migrator/migrations/migration-62.ts
@@ -12,7 +12,6 @@ export default {
         const { availableNetworks } = persistedState.NetworkController;
         const updatedNetworks = { ...availableNetworks };
 
-
         // Remove Scroll L1 testnet
         delete updatedNetworks['SCROLL_L1_TESTNET'];
 
@@ -23,7 +22,7 @@ export default {
             chainId: 534353,
             networkVersion: '534353',
             blockExplorerUrls: ['https://blockscout.scroll.io/'],
-        }
+        };
 
         // Add new zkSync Era Mainnet
         updatedNetworks.ZKSYNC_ERA_MAINNET = {
@@ -50,20 +49,21 @@ export default {
             defaultRpcUrl: `https://zksync-mainnet-node.blockwallet.io`,
             blockExplorerUrls: ['https://explorer.zksync.io/'],
             blockExplorerName: 'zkSync Explorer',
-            actionsTimeIntervals: { ...SLOW_TESTNET_TIME_INTERVALS_DEFAULT_VALUES },
+            actionsTimeIntervals: {
+                ...SLOW_TESTNET_TIME_INTERVALS_DEFAULT_VALUES,
+            },
             tornadoIntervals: {
                 depositConfirmations: 0,
                 derivationsForward: 0,
             },
             nativelySupported: true,
-        }
-
+        };
 
         // Update zkSync Era Testnet
         updatedNetworks.ZKSYNC_ALPHA_TESTNET = {
             ...updatedNetworks.ZKSYNC_ALPHA_TESTNET,
             desc: 'zkSync Era Testnet',
-            blockExplorerName: 'zkSync Testnet Explorer'
+            blockExplorerName: 'zkSync Testnet Explorer',
         };
 
         const orderedNetworks = normalizeNetworksOrder(updatedNetworks);
@@ -75,7 +75,6 @@ export default {
                 availableNetworks: { ...orderedNetworks },
             },
         };
-
     },
     version: '1.1.2',
 } as IMigration;

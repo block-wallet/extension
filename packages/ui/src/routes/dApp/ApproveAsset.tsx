add799919ac80905541818efe0651264cbf3edd6
Antony Ayoub <antony@blockwallet.io>
2023-03-16 16:44:54 +0200

    fix: invisible insufficient fund

--- packages/ui/src/routes/dApp/ApproveAsset.tsx
+++ packages/ui/src/routes/dApp/ApproveAsset.tsx
@@ -556,7 +556,7 @@ const ApproveAsset: FunctionComponent<ApproveAssetProps> = ({
                     buttonDisplay={false}
                     transactionId={transaction.id}
                 />
-                <div className="text-xs text-red-500">
+                <div className="text-xs text-red-500 !mt-0">
                     {!hasBalance && "Insufficient funds."}
                 </div>
             </div>

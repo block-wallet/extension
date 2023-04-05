806a9eead07ec02865d72b89eabfc34e612e2cec
Antony Ayoub <antony@blockwallet.io>
2023-04-05 18:52:20 +0200

    fix: svg warnings in network switch icon

--- packages/ui/src/components/icons/SwitchIcon.tsx
+++ packages/ui/src/components/icons/SwitchIcon.tsx
@@ -10,15 +10,15 @@ const SwitchIcon: React.FC<{ className?: string }> = (
             className={className}
             xmlns="http://www.w3.org/2000/svg"
         >
-            <g clip-path="url(#clip0_265_2)">
+            <g clipPath="url(#clip0_265_2)">
                 <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M11.1677 11.9997L7.14648 16.0137H9.96283L15.9961 9.99965H-3.91006e-05V11.9997H11.1677Z"
                 />
                 <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M4.82843 4.01402L8.84961 0H6.03327L0 6.01402H15.9961V4.01402H4.82843Z"
                 />
             </g>

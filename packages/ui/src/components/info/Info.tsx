019a34824f2dcbc117d19b4b4c9e58183e71cde3
Antony Ayoub <antony@blockwallet.io>
2023-04-17 20:36:19 +0200

    fix: release notes info title wrap

--- packages/ui/src/components/info/Info.tsx
+++ packages/ui/src/components/info/Info.tsx
@@ -24,7 +24,9 @@ const Info: FC<{ children: React.ReactNode }> & InfoComponents = ({
 }
 
 const Title = ({ children }: { children: React.ReactNode }) => {
-    return <span className="text-2xl font-bold leading-10  ">{children}</span>
+    return (
+        <span className="text-[1.4rem] font-bold leading-10  ">{children}</span>
+    )
 }
 
 const InfoList: FC<ClassNameProp & { children: React.ReactNode }> = ({

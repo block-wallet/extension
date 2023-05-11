5217a67784d7c2d08a90d9a27eb9bc144aaf5f2e
Rodrigo <10286502+rodr-r@users.noreply.github.com>
2023-05-11 16:08:51 -0300

    improving store management (#474)
    * improving store management
    
    * improving getAvailableChains

--- packages/background/src/controllers/OnrampController.ts
+++ packages/background/src/controllers/OnrampController.ts
@@ -29,10 +29,7 @@ export interface GetOnRampCurrencies {
  * If the target network hasn't been added to the user's network yet,
  * this controller stores references to recunstruct the transaction once the network is added.
  */
-export default class OnrampController extends BaseController<
-    undefined,
-    OnrampControllerMemState
-> {
+export default class OnrampController extends BaseController<OnrampControllerMemState> {
     constructor(private readonly _networkController: NetworkController) {
         super(undefined, { availableOnrampChains: [] });
 
@@ -44,7 +41,7 @@ export default class OnrampController extends BaseController<
      *
      */
     public getOnrampAvailableChains(): IChain[] {
-        return this.UIStore.getState().availableOnrampChains;
+        return this.store.getState().availableOnrampChains;
     }
 
     private _getAPIImplementation(
@@ -119,7 +116,7 @@ export default class OnrampController extends BaseController<
 
     public async getAvailableChains(
         aggregator: OnrampImplementation = OnrampImplementation.ONRAMPER_BUY
-    ): Promise<IChain[]> {
+    ) {
         const implementor = this._getAPIImplementation(aggregator);
         try {
             const onrampSupportedNetworks =
@@ -135,13 +132,11 @@ export default class OnrampController extends BaseController<
                 });
             });
 
-            this.UIStore.updateState({
+            this.store.setState({
                 availableOnrampChains: availableChains,
             });
-            return availableChains;
         } catch (e) {
             log.error('Error fetching onramper chains', e);
-            return [];
         }
     }
 }

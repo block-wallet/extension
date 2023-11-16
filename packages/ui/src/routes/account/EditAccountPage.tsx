3633c2d04592cc0a5c28bde34b2eef8a653112e5
Martin Lecam <martin.lecam@blockwallet.io>
2023-11-16 12:09:48 -0300

    Revert isFormValid validation for EditAccountPage

--- packages/ui/src/routes/account/EditAccountPage.tsx
+++ packages/ui/src/routes/account/EditAccountPage.tsx
@@ -102,6 +102,7 @@ const EditAccountPage = () => {
         <PopupLayout
             submitOnEnter={{
                 onSubmit,
+                isFormValid: Object.keys(errors).length === 0,
             }}
             header={
                 <PopupHeader

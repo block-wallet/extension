2025543329a56f3a71812b33dfbd1e7253686e3b
Rodrigo <10286502+rodr-r@users.noreply.github.com>
2024-10-10 13:39:26 -0300

    build

--- packages/background/src/controllers/KeyringControllerDerivated.ts
+++ packages/background/src/controllers/KeyringControllerDerivated.ts
@@ -8,7 +8,7 @@ import * as customEncryptor from '@metamask/browser-passworder';
 import { Hash, Hasheable } from '../utils/hasher';
 import { Mutex } from 'async-mutex';
 import LedgerBridgeKeyring from '@block-wallet/eth-ledger-bridge-keyring';
-import { TrezorKeyring } from '@block-wallet/eth-trezor-keyring';
+import TrezorKeyring from 'eth-trezor-keyring';
 import { Devices } from '../utils/types/hardware';
 import log from 'loglevel';
 import { HDPaths, BIP44_PATH } from '../utils/types/hardware';

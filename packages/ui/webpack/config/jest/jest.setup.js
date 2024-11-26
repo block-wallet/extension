ebe552d224afeecb464b31ecc1510baace8fb179
Rodrigo <10286502+rodr-r@users.noreply.github.com>
2024-11-25 23:52:33 -0300

    fix

--- packages/ui/webpack/config/jest/jest.setup.js
+++ packages/ui/webpack/config/jest/jest.setup.js
@@ -1,11 +1,2 @@
-import 'jest-canvas-mock';
-const { chrome } = require('jest-chrome')
-chrome.runtime.id = "testid";
-chrome.tabs.query = () => []
-chrome.runtime.sendMessage.mockImplementation(
-    (message, callback) => {
-        callback("response")
-    },
-)
-Object.assign(global, { chrome })
-
+Object.assign(global, require("jest-chrome"))
+import "jest-canvas-mock"
